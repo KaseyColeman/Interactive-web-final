e963d5a3057a8aef50b088ac4e64aab6c1d218dd
KaseyColeman <60412723+KaseyColeman@users.noreply.github.com>
2020-12-01 18:06:48 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const pug = require('pug');
+const express = require('express');
+const path = require('path');
+const routes = require('./routes/routes');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get('/', routes.index);
+app.get('/signup', routes.signup);
+
+
+app.listen(3001);
